81fd9c5d5c33184479830fa2a21a491f4f56b130
strwatcher <redmoonloon@outlook.com>
2023-03-15 23:57:56 +1000

    connect store to app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.scss'
+import 'normalize.css'
+import { Provider } from 'react-redux'
+import { store } from '@/store'
+import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
