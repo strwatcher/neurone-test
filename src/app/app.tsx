85ef69a81f34194959840444256dcbf1d4149dac
strwatcher <redmoonloon@outlook.com>
2023-03-16 11:16:24 +1000

    integrate main page into app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,29 +1,5 @@
-import { Button } from '@/shared/ui'
-import { useAppDispatch, useAppSelector } from '@/store'
-import { fetchTodoById } from '@/store/todo/model'
-import { useEffect } from 'react'
+import { Main } from '@/pages/main/main'
 
 export const App = () => {
-  const dispatch = useAppDispatch()
-  const { todo, status } = useAppSelector((state) => ({
-    todo: state.todo.todo,
-    status: state.todo.status,
-  }))
-
-  useEffect(() => {
-    console.log(todo)
-  }, [todo])
-
-  return (
-    <section>
-      <Button
-        text="Найти"
-        onClick={() => {
-          console.log('here')
-          dispatch(fetchTodoById(5))
-        }}
-      />
-      {status === 'loading' && 'Загрузка'}
-    </section>
-  )
+  return <Main />
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'normalize.css'
+import './index.scss'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
 import { App } from './app'
